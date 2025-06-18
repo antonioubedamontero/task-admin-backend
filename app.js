8dfc9e277445cec3573965360da92921ace4e7f9
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-06-18 09:24:41 +0200

    add task mock endpoints

--- app.js
+++ app.js
@@ -1,17 +1,125 @@
 const express = require('express');
 const app = express();
 
+
 // TODO: Temporary while including mongo
-const tasks = require('./mocks/tasks');
+let tasks = require('./mocks/tasks');
+const TaskState = require('./types/states.enum');
 
 // TODO: Include in .env
-const port = 3002;
+const port = 3005;
+
+// Middlewares
+app.use(express.json());  // for parsing application/json
 
 // TODO: Initial mock endpoint
 app.get('/tasks', (req,res) => {
-  console.log('tasks', tasks);
   res.status(200).json([...tasks]);
 });
+
+app.get('/tasks/:id', (req,res) => {
+  const { id } = req.params;
+  const taskById = tasks.find(task => task.id === id);
+  
+  if (taskById) {
+    return res.status(200).json(taskById)
+  }
+
+  res.status(404).json({ message: `Task ${id} not found`});
+});
+
+app.delete('/tasks/:id', (req,res) => {
+  const { id } = req.params;
+  const taskById = tasks.find(task => task.id === id);
+
+  if (taskById) {
+    tasks = tasks.filter( task => task.id !== id);
+    return res.status(200).json({ message: `Task ${id} deleted` });   
+  }
+
+  res.status(404).json({ message: `Task ${id} not found`});
+});
+
+app.post('/tasks', (req, res) => {
+  const { id, name, description, startDate, endDate } = req.body;
+  const existingTask = tasks.find(task => task.id === id);
+
+  if (existingTask) {
+    return res.status(409).json({ message: `Taks ${id} already exists`});
+  }
+
+  tasks.push({
+    id, 
+    name, 
+    description, 
+    startDate, 
+    endDate,
+    state: TaskState.STARTED,
+    logStates: [
+      {
+        startDate: new Date().toLocaleString(),
+        state: TaskState.STARTED,
+        endDate: null,
+        justification: ''
+      }
+    ]
+  });
+
+  res.status(201).json({ message: `Task ${id} created`});
+});
+
+app.patch('/tasks/:id', (req,res) => {
+  const { id } = req.params; 
+  const { name, description, currentState, justification } = req.body;
+
+  if (!id) {
+    return res.status(404).json({ message: `Task ${id} not exists`});
+  }
+
+  let taskById = tasks.find(task => task.id === id);
+
+  if (!taskById) {
+    return res.status(404).json({ message: `Task ${id} not exists`});
+  }
+
+  const logStates = taskById.logStates;
+
+  if (logStates.length > 0) {
+    logStates.at(-1).endDate = new Date().toLocaleString();
+  }
+
+  logStates.push({
+    startDate: new Date().toLocaleString(),
+    state: currentState,
+    endDate: null,
+    justification: justification ?? ''
+  })
+  
+  taskById = {
+    ...taskById,
+    name,
+    description,
+    currentState,
+    logStates
+  }
+
+  return res.status(200).json({
+    message: `Task ${id} updated`,
+    data: taskById
+  })
+});
+
+app.get('/tasks/state/:id', (req,res) => {
+  const { id } = req.params;
+  const tasksByState = tasks.filter(task => task.currentState === id);
+
+  if (!tasksByState) {
+    return res.status(404).json({ message: `Task ${id} not exists`});
+  }
+
+  res.status(200).json(tasksByState);ß
+});
+
 
 // Listen on port
 app.listen(port, (err) => {
--- mocks/tasks.js
+++ mocks/tasks.js
@@ -1,12 +1,12 @@
 const TaskState = require('../types/states.enum');
-const logStates = require('../mocks/log-state');
+const logStates = require('../mocks/log-states');
 
 const task1 = {
     id: '1',
     name: 'task1',
     description: 'Task1 description',
     startDate: '2025-06-17',
-    endDate: '2025-06-18',
+    dueDate: '2025-06-18',
     currentState: TaskState.BACKLOG,
     logStates: []
 };
@@ -16,7 +16,7 @@ const task2 = {
     name: 'task2',
     description: 'Task2 description',
     startDate: '2025-06-18',
-    endDate: '2025-06-25',
+    dueDate: '2025-06-25',
     currentState: TaskState.STARTED,
     logStates: [...logStates]
 };
@@ -26,7 +26,7 @@ const task3 = {
     name: 'task3',
     description: 'Task3 description',
     startDate: '2025-06-19',
-    endDate: '2025-06-20',
+    dueDate: '2025-06-20',
     currentState: TaskState.STARTED,
     logStates: []
 };
@@ -36,7 +36,7 @@ const task4 = {
     name: 'task4',
     description: 'Task4 description',
     startDate: '2025-06-20',
-    endDate: '2025-06-21',
+    dueDate: '2025-06-21',
     currentState: TaskState.PAUSED,
     logStates: []
 };
@@ -46,7 +46,7 @@ const task5 = {
     name: 'task5',
     description: 'Task5 description',
     startDate: '2025-06-21',
-    endDate: '2025-06-22',
+    dueDate: '2025-06-22',
     currentState: TaskState.ENDED,
     logStates: []
 };
@@ -56,7 +56,7 @@ const task6 = {
     name: 'task6',
     description: 'Task6 description',
     startDate: '2025-06-22',
-    endDate: '2025-06-23',
+    dueDate: '2025-06-23',
     currentState: TaskState.ENDED,
     logStates: []
 };
@@ -66,7 +66,7 @@ const task7 = {
     name: 'task7',
     description: 'Task7 description',
     startDate: '2025-06-23',
-    endDate: '2025-06-24',
+    dueDate: '2025-06-24',
     currentState: TaskState.CANCELED,
     logStates: []
 };
