ef8a3b049a9c279d7b4dbd0b0467593bc34894b4
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-06-29 17:15:20 +0200

    enable cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express');
-require('dotenv').config(); // Load vars from .env
+const express = require("express");
+const cors = require("cors");
 
+require("dotenv").config(); // Load vars from .env
 const app = express();
-const taskRoutes = require('./routes/task-controller-routes');
-const userRoutes = require('./routes/user-controller-routes');
-const connectMongoDb = require('./db/mongodb-connection');
+
+const taskRoutes = require("./routes/task-controller-routes");
+const userRoutes = require("./routes/user-controller-routes");
+const connectMongoDb = require("./db/mongodb-connection");
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,17 +14,17 @@ const PORT = process.env.PORT || 3000;
 connectMongoDb();
 
 // Middlewares
-app.use(express.json());  // for parsing application/json
+app.use(express.json()); // for parsing application/json
+app.use(cors()); // enable cors
 
 // Task Management routes
-app.use('/api/tasks', taskRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/users", userRoutes);
 
 // Listen on port
 app.listen(PORT, (err) => {
   if (err) {
-
-    return console.error('🔴 Error starting server: ', err);
+    return console.error("🔴 Error starting server: ", err);
   }
 
   console.info(`🟢 Task admin backend up and running on port ${PORT}`);
