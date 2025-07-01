75a9aea7b01cf24ba26325068eec2985ea37e280
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-07-01 08:28:05 +0200

    remove backlog state for task category

--- types/states.enum.js
+++ types/states.enum.js
@@ -1,10 +1,9 @@
 const TaskState = {
-    CREATED: 'created',
-    BACKLOG: 'backlog',
-    STARTED: 'started',
-    ENDED: 'ended',
-    PAUSED: 'paused',
-    CANCELED: 'canceled'
-}
+  CREATED: "created",
+  STARTED: "started",
+  ENDED: "ended",
+  PAUSED: "paused",
+  CANCELED: "canceled",
+};
 
 module.exports = TaskState;
